63d5ef989da91da4c0d4df08fb078cfbe8576f4b
forhot2000 <wx1983@gmail.com>
2015-04-24 18:24:46 +0800

    travis test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+    test "test user model" do
+        user = User.new(:first_name => 'Jamie')
+        assert_equal 'Jamie', user.first_name
+    end
 end
